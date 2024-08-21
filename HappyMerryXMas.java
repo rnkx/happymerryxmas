89c6bd957f94010619283bb14946dd898007f1c2
rnkx <rngkxxx@gmail.com>
2024-08-21 09:46:39 +0800

    Add files via upload

---
+++ HappyMerryXMas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.happymerryxmas;
+
+/**
+ *
+ * @author student
+ */
+public class HappyMerryXMas {
+
+    public static void main(String[] args) {
+        System.out.println("Merry Christmas");
+    }
+}
